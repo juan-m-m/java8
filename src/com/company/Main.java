3d0a907530a551313035bfa25de17f8208dfe237
juan-m-m <juan-es20@hotmail.com>
2019-08-15 00:47:18 -0400

    diferentes Formatos de la funcion lambda

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,5 +21,25 @@ public class Main {
         MiNombre miNombreLambda = () -> "juan lambda";
 
         System.out.println(miNombreLambda.miNombre());
+
+        Sumar sumar = new Sumar() {
+            @Override
+            public int suma(int a, int b) {
+                return a + b;
+            }
+        };
+
+        System.out.println(sumar.suma(1, 3));
+
+        Sumar suma1 = (a, b) -> a + b;
+        System.out.println(suma1.suma(1, 2));
+
+        Sumar suma2 = (a, b) -> {
+            a = b * b;
+            a = a + b;
+            System.out.println("mensaje dentro de lambda");
+            return a;
+        };
+        System.out.println(suma2.suma(1, 1));
     }
 }

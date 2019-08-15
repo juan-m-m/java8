22f4086797d0113a78259092a8c16e7cf18e0fe1
juan-m-m <juan-es20@hotmail.com>
2019-08-15 00:34:03 -0400

    first example function lambda

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        /*
+        ()->"mi nonbre"
+        (n)->n*n
+        (n)->n==2
+         */
+
+        MiNombre miNombreAnonima = new MiNombre() {
+            @Override
+            public String miNombre() {
+                return "Juan";
+            }
+        };
+        System.out.println(miNombreAnonima.miNombre());
+
+        MiNombre miNombreLambda = () -> "juan lambda";
+
+        System.out.println(miNombreLambda.miNombre());
     }
 }

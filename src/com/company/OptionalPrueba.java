8e5305904efd084c4a6da593479414ceddc26e45
juan-m-m <juan-es20@hotmail.com>
2019-08-18 16:42:35 -0400

    optional orElse

---
+++ src/com/company/OptionalPrueba.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Optional;
+
+/**
+ * @author Juan Montaño
+ */
+public class OptionalPrueba {
+
+    public static void main(String[] args) {
+        // pruebaOptional(null);
+        orElseOptional(null);
+    }
+
+    public static void pruebaOptional(String name) {
+        System.out.println(name.length());
+    }
+
+    public static void crearOptional() {
+        Optional<String> optional = Optional.empty();
+        optional.get();
+    }
+
+    public static void orElseOptional(String nombre) {
+        Optional<String> optional = Optional.ofNullable(nombre);
+        //Optional<String> optional1 = Optional.of(nombre);
+
+        String nombreOfNullable = optional.orElse("vacio");
+        //String nombreOF = optional1.orElse("vacio");
+
+        System.out.println(nombreOfNullable);
+        //System.out.println(nombreOF);
+    }
+}
